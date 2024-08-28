361575da0f99d05b61b912d2d094a5dc8aacf1ca
Sam-p2 <leumasolegnazianra@gmail.com>
2024-08-28 22:14:15 +0800

    Add files via upload

---
+++ ascii_art.py
@@ -0,0 +1,7 @@
+import pyfiglet
+
+# Customize this with your first name
+ascii_art = pyfiglet.figlet_format("Samuel", font="Sub-Zero")  # Replace 'Sub-Zero' with the style you chose
+print(ascii_art)
+
+
